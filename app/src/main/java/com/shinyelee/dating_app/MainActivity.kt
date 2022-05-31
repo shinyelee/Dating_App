0c5988a23a220b55e59fd19e9d7c22ec38b22a1d
shinyelee <shinye0213@naver.com>
2022-05-31 17:56:43 +0900

    CardStackView (2)

--- app/src/main/java/com/shinyelee/dating_app/MainActivity.kt
+++ app/src/main/java/com/shinyelee/dating_app/MainActivity.kt
@@ -2,10 +2,56 @@ package com.shinyelee.dating_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.shinyelee.dating_app.slider.CardStackAdapter
+import com.yuyakaido.android.cardstackview.CardStackLayoutManager
+import com.yuyakaido.android.cardstackview.CardStackListener
+import com.yuyakaido.android.cardstackview.CardStackView
+import com.yuyakaido.android.cardstackview.Direction
+import java.lang.StringBuilder
 
 class MainActivity : AppCompatActivity() {
+
+    // 효율적으로 데이터와 뷰를 관리하기 위해 어댑터가 필요함
+    lateinit var cardStackAdapter: CardStackAdapter
+    // 레이아웃 매니저로 뷰를 그려줌
+    lateinit var manager: CardStackLayoutManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val cardStackView = findViewById<CardStackView>(R.id.cardStackView)
+
+        manager = CardStackLayoutManager(baseContext, object: CardStackListener {
+
+            override fun onCardDragging(direction: Direction?, ratio: Float) {
+            }
+
+            override fun onCardSwiped(direction: Direction?) {
+            }
+
+            override fun onCardRewound() {
+            }
+
+            override fun onCardCanceled() {
+            }
+
+            override fun onCardAppeared(view: View?, position: Int) {
+            }
+
+            override fun onCardDisappeared(view: View?, position: Int) {
+            }
+
+        })
+        // 테스트
+        val testList = mutableListOf<String>()
+        testList.add("a")
+        testList.add("b")
+        testList.add("c")
+
+        cardStackAdapter = CardStackAdapter(baseContext, testList)
+        cardStackView.layoutManager = manager
+        cardStackView.adapter = cardStackAdapter
     }
 }
