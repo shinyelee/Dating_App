7322ee2542ca076bf9a0d0d41d39c372dc14ba3e
shinyelee <shinye0213@naver.com>
2022-06-01 21:25:07 +0900

    join page (1)

--- app/src/main/java/com/shinyelee/dating_app/auth/IntroActivity.kt
+++ app/src/main/java/com/shinyelee/dating_app/auth/IntroActivity.kt
@@ -1,12 +1,26 @@
 package com.shinyelee.dating_app.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.shinyelee.dating_app.R
 
 class IntroActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
+
+        val joinBtn = findViewById<Button>(R.id.joinBtn)
+
+        joinBtn.setOnClickListener {
+
+            val intent = Intent(this, JoinActivity::class.java)
+            startActivity(intent)
+
+        }
+
     }
 }
---
+++ app/src/main/java/com/shinyelee/dating_app/auth/JoinActivity.kt
@@ -0,0 +1,12 @@
+package com.shinyelee.dating_app.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.shinyelee.dating_app.R
+
+class JoinActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_join)
+    }
+}
