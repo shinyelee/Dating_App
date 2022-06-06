3b80e53f59192515fbdf26977df6bb9d72dc2820
shinyelee <shinye0213@naver.com>
2022-06-06 13:08:34 +0900

    Change profile image (4) Glide

--- app/src/main/java/com/shinyelee/dating_app/slider/CardStackAdapter.kt
+++ app/src/main/java/com/shinyelee/dating_app/slider/CardStackAdapter.kt
@@ -4,8 +4,13 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 import com.shinyelee.dating_app.R
 import com.shinyelee.dating_app.auth.UserDataModel
 
@@ -29,12 +34,25 @@ class CardStackAdapter(val context: Context, val items: List<UserDataModel>): Re
     // 자식뷰를 포함한 레이아웃 단위의 뷰를 하나의 뷰홀더로 설정
     inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
 
-        // 카드스택뷰의 별명, 지역, 나이
+        // 카드스택뷰의 프사, 별명, 지역, 나이
+        val image = itemView.findViewById<ImageView>(R.id.profileImageArea)
         val nickname = itemView.findViewById<TextView>(R.id.itemNickname)
         val city = itemView.findViewById<TextView>(R.id.itemCity)
         val age = itemView.findViewById<TextView>(R.id.itemAge)
 
         fun binding(data: UserDataModel) {
+
+            val storageRef = Firebase.storage.reference.child(data.uid + ".png")
+
+            storageRef.downloadUrl.addOnCompleteListener( OnCompleteListener { task ->
+
+                if(task.isSuccessful) {
+                    Glide.with(context)
+                        .load(task.result)
+                        .into(image)
+                }
+
+            })
 
             // UserDataModel의 데이터와 매칭
             nickname.text = data.nickname
