71dcd53e58804b2652e2702ecfc5836d05cd57ff
shinyelee <shinye0213@naver.com>
2022-06-07 00:51:28 +0900

    MyPageActivity (2)

--- app/src/main/java/com/shinyelee/dating_app/setting/MyPageActivity.kt
+++ app/src/main/java/com/shinyelee/dating_app/setting/MyPageActivity.kt
@@ -3,9 +3,15 @@ package com.shinyelee.dating_app.setting
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 import com.shinyelee.dating_app.R
 import com.shinyelee.dating_app.auth.UserDataModel
 import com.shinyelee.dating_app.utils.FirebaseAuthUtils
@@ -31,12 +37,43 @@ class MyPageActivity : AppCompatActivity() {
     // 파이어베이스에서 현재 사용자의 정보 불러오기
     private fun getMyData() {
 
+        // 프사, uid, 별명, 성별, 지역, 나이
+        val myImage = findViewById<ImageView>(R.id.myImage)
+//        val myUid = findViewById<TextView>(R.id.myUid)
+        val myNickname = findViewById<TextView>(R.id.myNickname)
+        val myGender = findViewById<TextView>(R.id.myGender)
+        val myCity = findViewById<TextView>(R.id.myCity)
+        val myAge = findViewById<TextView>(R.id.myAge)
+
         val postListener = object : ValueEventListener {
 
             // 데이터스냅샷 내 사용자 데이터 출력
             override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+                // 프사 제외한 나머지 정보
                 Log.d(TAG, dataSnapshot.toString())
                 val data = dataSnapshot.getValue(UserDataModel::class.java)
+
+                // uid, 별명, 성별, 지역, 나이
+//                myUid.text = data!!.uid
+                myNickname.text = data!!.nickname
+                myGender.text = data!!.gender
+                myCity.text = data!!.city
+                myAge.text = data!!.age
+
+                // 프사
+                val storageRef = Firebase.storage.reference.child(data.uid + ".png")
+
+                storageRef.downloadUrl.addOnCompleteListener( OnCompleteListener { task ->
+
+                    if(task.isSuccessful) {
+                        Glide.with(baseContext)
+                            .load(task.result)
+                            .into(myImage)
+                    }
+
+                })
+
             }
 
             override fun onCancelled(databaseError: DatabaseError) {
