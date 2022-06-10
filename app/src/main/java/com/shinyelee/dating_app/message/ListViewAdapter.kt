81117a69f26d50896ccade7909e61e6968fce796
shinyelee <shinye0213@naver.com>
2022-06-10 13:51:35 +0900

    ListViewAdapter (2) listViewItemNickname

--- app/src/main/java/com/shinyelee/dating_app/message/ListViewAdapter.kt
+++ app/src/main/java/com/shinyelee/dating_app/message/ListViewAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.TextView
 import com.shinyelee.dating_app.R
 import com.shinyelee.dating_app.auth.UserDataModel
 
@@ -21,6 +22,9 @@ class ListViewAdapter(val context : Context, val items : MutableList<UserDataMod
         if(convertView == null) {
             convertView = LayoutInflater.from(parent?.context).inflate(R.layout.list_view_item, parent, false)
         }
+        // 별명 불러와서 좋아요 리스트에 넣어줌
+        val nickname = convertView!!.findViewById<TextView>(R.id.listViewItemNickname)
+        nickname.text = items[position].nickname
         return convertView!!
     }
 
