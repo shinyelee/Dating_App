8de29ada813d0ed371f9d3c0dba34ea65d646e41
shinyelee <shinye0213@naver.com>
2022-06-14 21:32:25 +0900

    FirebaseService messaging

--- app/src/main/java/com/shinyelee/dating_app/message/fcm/FirebaseService.kt
+++ app/src/main/java/com/shinyelee/dating_app/message/fcm/FirebaseService.kt
@@ -1,4 +1,61 @@
 package com.shinyelee.dating_app.message.fcm
 
-class FirebaseService {
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import com.shinyelee.dating_app.R
+
+class FirebaseService : FirebaseMessagingService() {
+
+    private val TAG = "FirebaseService"
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+
+        super.onMessageReceived(message)
+
+//        Log.e(TAG, message.notification?.title.toString())
+//        Log.e(TAG, message.notification?.body.toString())
+        val title = message.notification?.title.toString()
+        val body = message.notification?.body.toString()
+
+        createNotificationChannel()
+        sendNotification(title, body)
+    }
+
+    // 알림 채널 시스템에 등록
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "name"
+            val descriptionText = "descriptionText"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel("CHANNEL_ID", name, importance).apply {
+                description = descriptionText
+            }
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    // 푸시 알림
+    private fun sendNotification(title : String, body : String) {
+        var builder = NotificationCompat.Builder(this, "CHANNEL_ID")
+            .setSmallIcon(R.drawable.logo)
+            .setContentTitle(title)
+            .setContentText(body)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        with(NotificationManagerCompat.from(this)) {
+            notify(1234, builder.build())
+        }
+    }
 }
