57dae6dd5a3dc25cbdb4ab624cf4372350bf54ec
shinyelee <shinye0213@naver.com>
2022-06-15 19:27:36 +0900

    Retrofit (1)

---
+++ app/src/main/java/com/shinyelee/dating_app/message/fcm/Repo.kt
@@ -0,0 +1,12 @@
+package com.shinyelee.dating_app.message.fcm
+
+class Repo {
+
+    companion object {
+
+        const val BASE_URL = "https://fcm.googleapis.com"
+        const val SERVER_KEY = ""
+        const val CONTENT_TYPE = "application/json"
+
+    }
+}
