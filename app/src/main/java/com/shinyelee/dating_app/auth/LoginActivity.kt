0a8e04666d4c1002b01c2453a47b9008ce4f29f6
shinyelee <shinye0213@naver.com>
2022-06-29 22:27:59 +0900

    login logic (1)

--- app/src/main/java/com/shinyelee/dating_app/auth/LoginActivity.kt
+++ app/src/main/java/com/shinyelee/dating_app/auth/LoginActivity.kt
@@ -3,15 +3,12 @@ package com.shinyelee.dating_app.auth
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.shinyelee.dating_app.MainActivity
 import com.shinyelee.dating_app.databinding.ActivityLoginBinding
-import java.util.regex.Pattern
 
 class LoginActivity : AppCompatActivity() {
 
@@ -34,108 +31,41 @@ class LoginActivity : AppCompatActivity() {
         // 파이어베이스 인증
         auth = Firebase.auth
 
-        // 이메일주소, 비밀번호
-        val emailText = binding.email.text.toString()
-        val pwText = binding.pw.text.toString()
-
-        // 이메일주소 정규식
-        val emailValidation = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$"
-
-        // 이메일주소 형식
-        fun emailPattern(string: String): Boolean {
-            val pattern = Pattern.matches(emailValidation, emailText)
-            return pattern
-        }
-
-        // 이메일주소 리스너
-        fun emailTextWatcher() {
-            binding.email.addTextChangedListener(object : TextWatcher {
-                override fun afterTextChanged(s: Editable?) {
-//                    if (s != null) {
-//                        when {
-//                            s.isEmpty() -> {
-//                                binding.emailArea.error = "이메일주소를 입력하세요"
-//                            }
-//                            !emailPattern(s.toString()) -> {
-//                                binding.emailArea.error = "이메일주소가 올바르지 않습니다"
-//                            }
-//                            else -> {
-//                                binding.emailArea.error = null
-//                            }
-//                        }
-//                    }
-                }
-                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                    if (s!!.isEmpty()) {
-                        binding.emailArea.error = "이메일주소를 입력하세요"
-                    } else if (!emailPattern(s.toString())) {
-                        binding.emailArea.error = "이메일주소가 올바르지 않습니다"
-                    } else {
-                        binding.emailArea.error = null
-                    }
-                }
-            })
-        }
-
-        // 비밀번호 리스너
-        fun pwTextWatcher() {
-            binding.pw.addTextChangedListener(object : TextWatcher {
-                override fun afterTextChanged(s: Editable?) {
-//                    if (s != null) {
-//                        when {
-//                            s.isEmpty() -> {
-//                                binding.pwArea.error = "비밀번호를 입력하세요"
-//                            }
-//                            s.isNotEmpty() -> {
-//                                when {
-//                                    pwText.length<6 -> {
-//                                        binding.pwArea.error = "최소 6자리 이상 입력하세요"
-//                                    }
-//                                    pwText.length>20 -> {
-//                                        binding.pwArea.error = "20자리 이하로 입력하세요"
-//                                    }
-//                                    else -> {
-//                                        binding.pwArea.error = null
-//                                    }
-//                                }
-//                            }
-//                            else -> {
-//                                binding.pwArea.error = "비밀번호를 다시 확인하세요"
-//                            }
-//                        }
-//                    }
-                }
-                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                    if (s!!.isEmpty()) {
-                        binding.pwArea.error = "비밀번호를 입력하세요"
-                    } else if (pwText.length<6) {
-                        binding.pwArea.error = "최소 6자리 이상 입력하세요"
-                    } else if (pwText.length>20) {
-                        binding.pwArea.error = "20자리 이하로 입력하세요"
-                    } else {
-                        binding.pwArea.error = null
-                    }
-                }
-            })
-        }
-
-        emailTextWatcher()
-        pwTextWatcher()
-
         // 로그인 버튼
         binding.loginBtn.setOnClickListener {
-            // 조건 모두 만족하면 로그인
-            auth.signInWithEmailAndPassword(emailText, pwText)
-                .addOnCompleteListener(this) { task ->
-                    if (task.isSuccessful) {
-                        val intent = Intent(this, MainActivity::class.java)
-                        startActivity(intent)
-                    } else {
-                        Toast.makeText(this, "이메일주소와 비밀번호를 다시 확인하세요", Toast.LENGTH_SHORT).show()
+
+            // 로그인조건 확인
+            var emailCheck = true
+            var pwCheck = true
+            var loginCheck = emailCheck && pwCheck
+
+            // 메일주소, 비밀번호
+            val emailTxt = binding.email.text.toString()
+            val pwTxt = binding.pw.text.toString()
+
+            // 빈 칸 검사
+            if(emailTxt.isEmpty()) {
+                emailCheck = false
+                binding.emailArea.error = "이메일주소를 입력해 주세요"
+            }
+            if(pwTxt.isEmpty()) {
+                pwCheck = false
+                binding.pwArea.error = "비밀번호를 입력해 주세요"
+            }
+
+            // 로그인조건 모두 만족하면 로그인
+            if(loginCheck) {
+                auth.signInWithEmailAndPassword(emailTxt, pwTxt)
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            val intent = Intent(this, MainActivity::class.java)
+                            startActivity(intent)
+                        } else {
+                            Toast.makeText(this, "로그인 실패", Toast.LENGTH_SHORT).show()
+                        }
                     }
-                }
+            }
+
         }
 
     }
